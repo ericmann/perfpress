73374612235304e51cf9c80b2ce4b3fd9d52a854
Eric Mann <eric@eamann.com>
2016-01-07 16:46:18 -0800

    Cached vs Uncached WP_Query
    Test the memory performance impact of using a cached WP_Query object vs a
    new instance on every iteration of a loop across the entire database.

--- perfpress.php
+++ perfpress.php
@@ -76,6 +76,46 @@ class PerfPress_Command extends WP_CLI_Command {
 	}
 
 	/**
+	 * Test the performance of creating a WP_Query inside a loop versus outside a loop.
+	 *
+	 * ## OPTIONS
+	 *
+	 * [--posts=<count>]
+	 * : Number of post objects to test with int he database
+	 *
+	 * [--batch=<count>]
+	 * : Number of posts to use as a batch
+	 *
+	 * [--loopback=<url>]
+	 * : Remove server to ping with statistics instead of printing directly to CLI
+	 *
+	 * ## EXAMPLES
+	 *
+	 *    wp perfpress wp_query --posts=10000
+	 *    wp perfpress wp_query --loopback=http://localhost:8888
+	 *
+	 * @synopsis [--posts=<count>] [--batch=<count>] [--loopback=<url>]
+	 * @alias q
+	 *
+	 * @param array $args
+	 * @param array $assoc_args
+	 */
+	public function wp_query( $args, $assoc_args ) {
+		$posts = isset( $assoc_args['posts'] ) ? intval( $assoc_args['posts'] ) : 10000;
+		$batch = isset( $assoc_args['batch'] ) ? intval( $assoc_args['batch'] ) : 10;
+		$loopback = isset( $assoc_args['loopback'] ) ? $assoc_args['loopback'] : false;
+
+		// Add our posts to the database
+		$this->setup( $posts, false );
+
+		// First do a stored batch
+		$this->cached_batch( $batch, $loopback );
+
+		// Now do an uncached batch
+		$this->uncached_batch( $batch, $loopback );
+	}
+
+	/**
 	 * Output the test results
 	 *
 	 * @param array $results
@@ -122,8 +162,8 @@ class PerfPress_Command extends WP_CLI_Command {
 	/**
 	 * Set up the database environment with a certain number of posts
 	 *
-	 * @param array $count     Number of posts with which to populate the database
-	 * @param bool  $add_pages Flag whether or not to add pages in addition to posts
+	 * @param int  $count     Number of posts with which to populate the database
+	 * @param bool $add_pages Flag whether or not to add pages in addition to posts
 	 */
 	protected function setup( $count, $add_pages ) {
 		global $wpdb;
@@ -146,7 +186,10 @@ class PerfPress_Command extends WP_CLI_Command {
 				'post_type'    => 'post',
 			];
 
-			wp_insert_post( $post );
+			$new_id = wp_insert_post( $post );
+			if ( $new_id ) {
+				clean_post_cache( $new_id );
+			}
 
 			if ( $add_pages ) {
 				$post['post_type'] = 'page';
@@ -204,6 +247,155 @@ class PerfPress_Command extends WP_CLI_Command {
 		$total = microtime( true ) - $timer;
 
 		return (string) $total;
+	}
+
+	/**
+	 * Loop through all posts with a cached WP_Query instance.
+	 *
+	 * @global wpdb $wpdb
+	 *
+	 * @param int         $batch
+	 * @param string|bool $loopback
+	 */
+	protected function cached_batch( $batch, $loopback ) {
+		global $wpdb;
+
+		$post_count = wp_count_posts( 'post' );
+		$total = $post_count->publish;
+
+		$this->print_memory( 'Before cached run' );
+
+		$progress = new \cli\progress\Bar( 'Processing with a cached WP_Query', $total );
+		$progress->display();
+
+		// Set up batch args
+		$page = 1;
+		$migrated = 0;
+
+		$query = new WP_Query();
+		do {
+			$loopback && $this->print_loopback( $loopback, 'cached' );
+			$query->query( [
+				'post_type'      => 'post',
+				'posts_per_page' => $batch,
+				'paged'          => $page,
+				'post_status'    => 'publish',
+				'no_found_rows'  => true,
+			] );
+
+			$count = count( $query->posts );
+			$migrated += $count;
+
+			foreach( $query->posts as $post ) {
+				// This is where we'd do some magic in a migration
+
+				$progress->tick();
+
+				// Clean up our cache as we go (so the next batch will be clear)
+				clean_post_cache( $post->ID );
+			}
+
+			// Free memory
+			$wpdb->flush();
+
+			// Next page
+			$page += 1;
+		} while ( $count );
+
+		$progress->finish();
+
+		$this->print_memory( 'After cached run' );
+	}
+
+	/**
+	 * Loop through all posts with a distinct WP_Query instances.
+	 *
+	 * @global wpdb $wpdb
+	 *
+	 * @param int         $batch
+	 * @param string|bool $loopback
+	 */
+	protected function uncached_batch( $batch, $loopback ) {
+		global $wpdb;
+
+		$post_count = wp_count_posts( 'post' );
+		$total = $post_count->publish;
+
+		$this->print_memory( 'Before cached run' );
+
+		$progress = new \cli\progress\Bar( 'Processing with an uncached WP_Query', $total );
+		$progress->display();
+
+		// Set up batch args
+		$page = 1;
+		$migrated = 0;
+
+		do {
+			$loopback && $this->print_loopback( $loopback, 'uncached' );
+			$query = new WP_Query([
+				'post_type'      => 'post',
+				'posts_per_page' => $batch,
+				'paged'          => $page,
+				'post_status'    => 'publish',
+				'no_found_rows'  => true,
+			] );
+
+			$count = count( $query->posts );
+			$migrated += $count;
+
+			foreach( $query->posts as $post ) {
+				// This is where we'd do some magic in a migration
+
+				$progress->tick();
+
+				// Clean up our cache as we go (so the next batch will be clear)
+				clean_post_cache( $post->ID );
+			}
+
+			// Free memory
+			$wpdb->flush();
+
+			// Next page
+			$page += 1;
+		} while ( $count );
+
+		$progress->finish();
+
+		$this->print_memory( 'After uncached run' );
+	}
+
+	/**
+	 * Print our our memory usage
+	 *
+	 * @param string [$tag]
+	 */
+	protected function print_memory( $tag = '' ) {
+		$mem = ( memory_get_peak_usage(false) / 1024 / 1024 ) . " MiB";
+
+		if ( ! empty( $tag ) ) {
+			$mem .= ' | ' . $tag;
+		}
+
+		WP_CLI::line( sprintf( 'Memory usage: %s', $mem ) );
+	}
+
+	/**
+	 * Print memory usage to a loopback listener.
+	 *
+	 * @param string $loopback
+	 * @param string [$tag]
+	 */
+	protected function print_loopback( $loopback, $tag = '' ) {
+		$mem = ( memory_get_peak_usage(false) / 1024 / 1024 ) . " MiB";
+
+		if ( ! empty( $tag ) ) {
+			$mem .= ' | ' . $tag;
+		}
+
+		wp_remote_post( $loopback, array(
+			'blocking' => false,
+			'body'     => $mem,
+		) );
 	}
 }
 
